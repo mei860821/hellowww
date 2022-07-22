b4ee84ea5e5ceeda46c026115559d98e12b98b62
rosiehung821 <rosiehung821@gmail.com>
2022-07-22 15:34:26 +0800

    draw

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <KonvaBox></KonvaBox> -->
+    <v-stage
+      ref="stage"
+      :config="stageConfig"
+      @mousemove="handleMouseMove"
+      @touchmove="handleMouseMove"
+      @mouseDown="handleMouseDown"
+      @touchstart="handleMouseDown"
+      @mouseUp="handleMouseUp"
+      @touchend="handleMouseUp"
+    >
+      <v-layer ref="layer">
+        <v-circle :config="configCircle"></v-circle>
+      </v-layer>
+    </v-stage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue";
+import VueKonva from "vue-konva";
+import Konva from "konva";
+
+Vue.use(VueKonva);
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      configKonva: {
+        width: 200,
+        height: 200,
+      },
+      configCircle: {
+        x: 100,
+        y: 100,
+        radius: 70,
+        fill: "red",
+        stroke: "black",
+        strokeWidth: 4,
+      },
+      stageConfig: {
+        draggable: false,
+        x: 0,
+        y: 0,
+        width: 500,
+        height: 500,
+      },
+      isPaint: false,
+      mode: "brush",
+      lastLine: null,
+    };
+  },
+  methods: {
+    handleMouseDown() {
+      const layer = this.$refs.layer.getNode();
+      this.isPaint = true;
+      const pos = this.$refs.stage.getNode().getPointerPosition();
+      // var pos = stage.getPointerPosition();
+      this.lastLine = new Konva.Line({
+        stroke: "#df4b26",
+        strokeWidth: 5,
+        globalCompositeOperation:
+          this.mode === "brush" ? "source-over" : "destination-out",
+        points: [pos.x, pos.y],
+      });
+      layer.add(this.lastLine);
+    },
+    handleMouseMove() {
+      if (!this.isPaint) {
+        return;
+      }
+
+      const layer = this.$refs.layer.getNode();
+      // const pos = stage.getPointerPosition();
+      const pos = this.$refs.stage.getNode().getPointerPosition();
+
+      var newPoints = this.lastLine.points().concat([pos.x, pos.y]);
+      this.lastLine.points(newPoints);
+      layer.batchDraw();
+    },
+    handleMouseUp() {
+      this.isPaint = false;
+    },
+  },
+  mounted() {
+    this.stageConfig.height = window.innerHeight;
+    this.stageConfig.width = window.innerWidth;
+  },
+};
 </script>
 
 <style>
@@ -24,5 +102,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 500px;
+  height: 500px;
 }
 </style>
