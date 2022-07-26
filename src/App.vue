cb29a68c8fa21079f5e38575727939eefb204609
rosiehung821 <rosiehung821@gmail.com>
2022-07-26 10:39:24 +0800

    clear poo

--- src/App.vue
+++ src/App.vue
@@ -22,74 +22,19 @@
 </template>
 
 <script>
-import Vue from "vue";
-import VueKonva from "vue-konva";
+// import Vue from "vue";
+// import VueKonva from "vue-konva";
 import Konva from "konva";
 
-Vue.use(VueKonva);
+// Vue.use(VueKonva);
 
 export default {
   name: "App",
   components: {},
   data() {
-    return {
-      configKonva: {
-        width: 200,
-        height: 200,
-      },
-      configCircle: {
-        x: 100,
-        y: 100,
-        radius: 70,
-        fill: "red",
-        stroke: "black",
-        strokeWidth: 4,
-      },
-      stageConfig: {
-        draggable: false,
-        x: 0,
-        y: 0,
-        width: 500,
-        height: 500,
-      },
-      isPaint: false,
-      mode: "brush",
-      lastLine: null,
-    };
+    return {};
   },
-  methods: {
-    handleMouseDown() {
-      const layer = this.$refs.layer.getNode();
-      this.isPaint = true;
-      const pos = this.$refs.stage.getNode().getPointerPosition();
-      // var pos = stage.getPointerPosition();
-      this.lastLine = new Konva.Line({
-        stroke: "#df4b26",
-        strokeWidth: 5,
-        tension: 0.5,
-        globalCompositeOperation:
-          this.mode === "brush" ? "source-over" : "destination-out",
-        points: [pos.x, pos.y],
-      });
-      layer.add(this.lastLine);
-    },
-    handleMouseMove() {
-      if (!this.isPaint) {
-        return;
-      }
-
-      const layer = this.$refs.layer.getNode();
-      // const pos = stage.getPointerPosition();
-      const pos = this.$refs.stage.getNode().getPointerPosition();
-
-      var newPoints = this.lastLine.points().concat([pos.x, pos.y]);
-      this.lastLine.points(newPoints);
-      layer.batchDraw();
-    },
-    handleMouseUp() {
-      this.isPaint = false;
-    },
-  },
+  methods: {},
   mounted() {
     // this.stageConfig.height = window.innerHeight;
     // this.stageConfig.width = window.innerWidth;
